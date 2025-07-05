f96bb116356dc49ebe1e3f0fcc330ad3e99975e2
BlockbyJamez <danny90628@gmail.com>
2025-07-05 23:22:59 +0800

    init front-end only

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import ForgotPassword from "@/views/ForgotPassword.vue";
@@ -38,7 +38,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes,
 });
 
